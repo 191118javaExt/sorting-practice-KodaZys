cb7dbef0623312e85977b629aaab2cd5f8bc34ad
Sophia <msophiagavrila@gmail.com>
2019-12-05 01:09:28 -0500

    Completed MergeSort && BubbleSort

--- SortPractice/src/com/revature/sort/BubbleSort.java
+++ SortPractice/src/com/revature/sort/BubbleSort.java
@@ -3,6 +3,14 @@ package com.revature.sort;
 public class BubbleSort {
 	
 	public static void sort(int[] arr) {
-		// TODO Auto-generated method stub
+	
+		for(int i=0; i<arr.length-1; i++)
+			for(int j=0; j < arr.length-i-1; j++)
+				if(arr[j] > arr[j+1]) {
+					// make temp array for j values
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+		}
 	}
 }
--- SortPractice/src/com/revature/sort/MergeSort.java
+++ SortPractice/src/com/revature/sort/MergeSort.java
@@ -2,11 +2,51 @@ package com.revature.sort;
 
 public class MergeSort {
 	
+	// O(n log n) comparison-based sorting algorithm
+	// **** Divide and Conquer ****
+	// find middle --> split into left and right parts --> RECURSIVELY SPLIT!
+	// merge back together
+
 	public static void sort(int[] arr) {
-		// TODO Auto-generated method stub
+		
+		int n = arr.length;
+		if(n < 2) {
+			return;
+		}
+		int mid = n/2;
+		int[] left = new int[mid];
+		int[] right = new int[n-mid];
+		
+		// pop l
+		for(int i=0; i<mid; i++) {
+			left[i] = arr[i];
+		}
+		// pop r
+		for(int i=0; i<right.length; i++) {
+			right[i] = arr[n/2 + i];
+		}
+		
+		sort(left);
+		sort(right);
+		//invoke private merge method
+		merge(arr, left, right);
 	}
 	
 	private static void merge(int[] arr, int[] left, int[] right) {
-		// TODO Auto-generated method stub
+		
+		int i = 0;
+		int j = 0;
+		int n = arr.length;
+		
+		for(int k=0; k<n; k++) {
+			
+			if  (i >= left.length) {
+				arr[k] = right[j++];
+			} else if (j >= right.length || left[i] <= right[j]) {
+				arr[k] = left[i++]; 
+			} else {
+				arr[k] = right[j++];
+			}
+		}
 	}
 }
